8c3ba1c4d2fdb6c42d2fe229ce0e67816979298d
MaryOleks <mariia.oleksiuk@omnigon.com>
2019-11-04 16:36:23 +0200

    add boilerplate

---
+++ src/bus/__DOMAIN__/actions.js
@@ -0,0 +1,9 @@
+import { types } from './types';
+
+export const domainActions = Object.freeze({
+  domainAction: () => {
+    return {
+      type: types.DOMAIN_ACTION
+    }
+  }
+});
---
+++ src/bus/__DOMAIN__/reducer.js
@@ -0,0 +1,15 @@
+import { types } from './types';
+
+const initialState = {
+  property: ''
+};
+
+export const domainReducer = ( state = initialState, { type } ) => {
+  switch(type) {
+    case types.DOMAIN_ACTION:
+      return { ... state, property: '' };
+
+    default:
+      return state;
+  }
+};
